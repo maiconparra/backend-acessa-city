014e81c2b3a86c4ffbfdd1c52c31becca71eb58d
Thales Reis <thales.areis@gmail.com>
2020-05-18 23:58:15 -0300

    add more cityhall info on dto

--- src/AcessaCity.API/Dtos/CityHall/CityHallDto.cs
+++ src/AcessaCity.API/Dtos/CityHall/CityHallDto.cs
@@ -11,6 +11,8 @@ namespace AcessaCity.API.Dtos.CityHall
         public Guid CityId { get; set; }        
         public City City { get; set; }
         public string Address { get; set; }
+        public string Number { get; set; }
+        public string ZIPCode { get; set; }
         public string Neighborhood { get; set; }
 
         public bool Verified { get; set; }
